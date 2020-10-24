6a37a932933a4c536363f63cfca7f90b586d40de
semishawn <45698497+semishawn@users.noreply.github.com>
2020-10-24 05:07:47 -0400

    small changes whatever im tired

--- js/index.js
+++ js/index.js
@@ -1,22 +1,22 @@
-$(document).ready(function(){
-	$('.searchbar').on("keyup", function() {
+$(document).ready(function() {
+	$('.searchbar').on('keyup', function() {
 		$('.search-results').css('display', 'block');
 		var value = $(this).val().toLowerCase();
 		$('.search-results > a').filter(function() {
 			$(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
 			$('.search-results > a:visible:gt(4)').hide();
 		});
-		if( !$(this).val() ) {
+		if (!$(this).val()) {
 			$('.search-results').hide();
 		}
 	});
 
-	$('.searchbar').bind("enterKey",function(e) {
+	$('.searchbar').bind('enterKey', function(e) {
 		$('.search-results > a:visible').get(0).click();
 	});
 	$('.searchbar').keyup(function(e) {
-		if(e.keyCode == 13) {
-			$(this).trigger("enterKey");
+		if (e.keyCode == 13) {
+			$(this).trigger('enterKey');
 		}
 	});
 });
--- js/main.js
+++ js/main.js
@@ -1,35 +1,10 @@
-$('li>a, p>a').not(".post-link").addClass('inline');
-$('p>a').not(".404-link").attr('target', '_blank');
+$('li > a, p > a').not('.post-link').addClass('inline');
+$('p > a').not('.404-link').attr('target', '_blank');
 
 
-$(".ribbon").on({
+$('.ribbon').on({
 	mouseenter: function() {$('.base-body > .popup').css('opacity', '0.5');},
 	mouseleave: function() {$('.base-body > .popup').css('opacity', '1');}
-});
-
-
-$('.navicon>label').click(function() {
-	if ($('#toggle').is(':checked')) { 
-		$('.top').css({
-			'transform': 'rotate(0)',
-			'top': 'calc(var(--a)/8)'
-		});
-		$('.mid').css({'transform': 'scale(1)'});
-		$('.bot').css({
-			'transform': 'rotate(0)',
-			'bottom': 'calc(var(--a)/8)'
-		});
-	} else {
-		$('.top').css({
-			'transform': 'rotate(45deg)',
-			'top': 'calc(var(--a)/2 - var(--b)/2)'
-		});
-		$('.mid').css({'transform': 'scale(0)'});
-		$('.bot').css({
-			'transform': 'rotate(-45deg)',
-			'bottom': 'calc(var(--a)/2 - var(--b)/2)'
-		});
-	}
 });
 
 
